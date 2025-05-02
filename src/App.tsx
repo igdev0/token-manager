c77e938ac7f77daffc756923319abe51e7d82eaa
igdev <dorultanianos@gmail.com>
2025-05-02 17:30:33 +0300

    feat: install basic radix components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import {Button} from '@/components/ui/button.tsx';
+import {Wallet} from 'lucide-react';
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className="container mx-auto">
+      <Button variant="default">Connect <Wallet/></Button>
+    </div>
   )
 }
 
