87afcb1fa18ed780dc84b5f35bf4aa0e46fea389
igdev <dorultanianos@gmail.com>
2025-05-21 10:12:36 +0300

    feat: add decimals as configurable in constructor

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
-    constructor(uint256 initialSupply, string memory  _name, string memory _symbol) ERC20(_name, _symbol) {
+    uint8 private _decimals;
+    constructor(uint256 initialSupply, string memory  _name, string memory _symbol, uint8 _dec) ERC20(_name, _symbol) {
         _mint(msg.sender, initialSupply);
+        _decimals = _dec;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
     }
 }
