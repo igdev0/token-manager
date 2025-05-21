6e8c60206c3b896ee9f92742b554e815d0cbb306
igdev <dorultanianos@gmail.com>
2025-05-21 13:03:32 +0300

    test: token should be ownable

--- test/ERC20Token.ts
+++ test/ERC20Token.ts
@@ -1,4 +1,4 @@
-import {ethers} from 'hardhat';
+import hre from 'hardhat';
 import {loadFixture} from '@nomicfoundation/hardhat-network-helpers';
 import assert from 'node:assert';
 
@@ -8,23 +8,23 @@ describe('ERC20Token', () => {
   async function deployContractFixture() {
     const name = 'Test Token';
     const symbol = 'TT';
-    const totalSupply = 1000000;
+    const initialSupply = 1000000;
     const decimals = 18;
 
-    const [owner] = await ethers.getSigners();
-    const contractFactory = await ethers.getContractFactory('ERC20Token');
-    const contract = await contractFactory.deploy(name, symbol, totalSupply, decimals);
+    const [owner] = await hre.ethers.getSigners();
+    const contractFactory = await hre.ethers.getContractFactory('ERC20Token');
+    const contract = await contractFactory.deploy(name, symbol, String(initialSupply), decimals);
     const deployed = await contract.waitForDeployment();
     return {
       address: await deployed.getAddress(),
       contract: contract,
-      owner
+      owner: await owner.getAddress(),
     }
   }
 
-  it('should be able to deploy a new token', async () => {
-    const {address, owner, contract} = await loadFixture(deployContractFixture);
-
-    assert(owner === contract.getOwner());
+  it('It should be owned by the signer ', async () => {
+    const {address, owner} = await loadFixture(deployContractFixture);
+    const contract = await hre.ethers.getContractAt('ERC20Token', address);
+    assert(await contract.owner.staticCall() === owner, "Owner is set incorrectly");
   });
 })
