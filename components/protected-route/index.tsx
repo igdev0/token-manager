fe44e88b819bd3bed347b566b255442ccc2514e2
igdev <dorultanianos@gmail.com>
2025-05-21 13:13:17 +0300

    refactor: remove unnecessary protected-route component

--- components/protected-route/index.tsx
+++
@@ -1,7 +0,0 @@
-import {useWalletStore} from '@/store/wallet.ts';
-import {Navigate, Outlet} from 'react-router';
-
-export default function ProtectedRoute() {
-  const wallet = useWalletStore();
-  return wallet.authenticated ? <Outlet/> : <Navigate to="/"/>
-}
