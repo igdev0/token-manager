4cb77bd31a4beb5864e8d973df4bdaade8460779
igdev <dorultanianos@gmail.com>
2025-05-22 12:18:11 +0300

    feat: add Prisma schema and initial migration for Contact model

---
+++ prisma/migrations/20250522091604_create_contact_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" TEXT NOT NULL,
+    "alias" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
