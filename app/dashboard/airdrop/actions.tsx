e1f27a4d79398de2a40b0b997b74b6a90cbca923
igdev <dorultanianos@gmail.com>
2025-05-25 20:50:33 +0300

    feat: add actions for fetching tokens and contacts from the database

---
+++ app/dashboard/airdrop/actions.tsx
@@ -0,0 +1,20 @@
+"use server";
+import {PrismaClient} from '@/lib/generated/prisma';
+
+const client = new PrismaClient();
+
+export async function getTokens(owner: string) {
+  return client.token.findMany({where: {owner}});
+}
+
+export async function getContactsByID(id: string, owner: string) {
+  return client.contact.findMany({where: {id, owner}});
+}
+
+export async function getContactAddressesByTags(tags: string[], owner: string) {
+  return client.contact.findMany({where: {owner, tags: {hasSome: tags}}}).then(res => Array.from(new Set(res.map(r => r.address))));
+}
+
+export async function getContactTags(owner: string) {
+  return client.contact.findMany({where: {owner}}).then(res => Array.from(new Set(res.flatMap(r => r.tags))));
+}
