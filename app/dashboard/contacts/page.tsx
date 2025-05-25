c6e0e0f59d396fe4086ee496339aa42b32718a30
igdev <dorultanianos@gmail.com>
2025-05-25 14:21:34 +0300

    feat: enhance Contacts page layout with improved table structure and styling

--- app/dashboard/contacts/page.tsx
+++ app/dashboard/contacts/page.tsx
@@ -30,11 +30,29 @@ export default function Page() {
     )
   }
 
-  return (
-      <div>
-        <h1 className="text-2xl font-extrabold">Contacts</h1>
-        <table>
+  const thCls = "px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider";
 
+  return (
+      <div className="w-full">
+        <h1 className="text-2xl font-extrabold">Contacts</h1>
+        <table className="w-full">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className={thCls}>Alias</th>
+              <th className={thCls}>Address</th>
+              <th className={thCls}>Tags</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            data.map(({id, alias, address, tags}) => (
+                <tr key={id}>
+                  <td className="px-4 py-2">{alias}</td>
+                  <td className="px-4 py-2">{address}</td>
+                  <td className="px-4 py-2 flex gap-2">{tags.map(tag => (<span key={tag} className="px-2 py-1 text-sm bg-black text-white rounded-sm">{tag}</span>))}</td>
+                </tr>))
+          }
+          </tbody>
         </table>
       </div>
   );
