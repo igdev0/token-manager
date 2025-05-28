b287d04f1e0f07888de7784d7416f7a82715a361
igdev <dorultanianos@gmail.com>
2025-05-28 16:55:16 +0300

    fix: prisma deployment issue

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-
-  },
+  serverExternalPackages: ["@prisma/client"],
   eslint: {
     ignoreDuringBuilds: true,
   }
