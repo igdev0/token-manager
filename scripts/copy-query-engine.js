269da42d88e83bb9dad453122be736c0a57a2bb1
igdev <dorultanianos@gmail.com>
2025-05-29 12:44:53 +0300

    wip: fix prisma deployment issue

--- scripts/copy-query-engine.js
+++ scripts/copy-query-engine.js
@@ -1,18 +1,33 @@
 // scripts/copy-query-engine.js
 import fs from 'fs';
 import path from 'node:path';
+import * as os from 'node:os';
 // Source location of the query engine binary
+let filename;
+
+console.log(os.platform());
+switch (os.platform()) {
+    case "darwin":
+        filename = "libquery_engine-darwin-arm64.dylib.node";
+        break;
+    case "linux":
+        filename = "libquery_engine-rhel-openssl-3.0.x.so.node";
+        break;
+    default:
+        throw new Error(`Unsupported version: ${os.version()}`);
+}
+
 const sourcePath = path.resolve(
-    'node_modules/@prisma/engines/libquery_engine-rhel-openssl-3.0.x.so.node'
+    `${process.cwd()}/node_modules/@prisma/engines/${filename}`
 );
 
 // Destination (adjust if needed)
-const destDir = path.resolve(`/var/task/lib/generated/prisma`);
-const destPath = path.join(destDir, 'libquery_engine-rhel-openssl-3.0.x.so.node');
+const destDir = path.resolve(`${process.cwd()}/lib/generated/prisma`);
+const destPath = path.join(destDir, filename);
 
 // Ensure the destination directory exists
 if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir, { recursive: true });
+    fs.mkdirSync(destDir, {recursive: true});
 }
 
 // Copy the binary
